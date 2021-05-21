1775e9551d1841810cca9872ff22d7e35d8688e4
Daniel <mrpaladin3@gmail.com>
2021-05-21 13:52:37 +0500

    Первый шаг к выводу текста

--- Views/EntityDescription.cs
+++ Views/EntityDescription.cs
@@ -7,6 +7,7 @@ namespace Cave_Adventure.Views
     public partial class EntityDescription : Form
     {
         private readonly Entity _entity;
+        private readonly Label _infoLabel;
         private bool _configured = false;
 
         protected override void OnLoad(EventArgs e)
@@ -22,11 +23,23 @@ namespace Cave_Adventure.Views
         {
             _entity = entity;
             InitializeComponent();
+            _infoLabel = new Label
+            {
+                Text = $"{WriteEntityDescription()}",
+                ForeColor = Color.Black,
+                Size = this.Size,
+                // Margin = new Padding(10, 0, 0, 0)
+            };
         }
         
         public void Configure(ArenaFieldControl arenaFieldControl)
         {
             _configured = true;
+        }
+
+        private string WriteEntityDescription()
+        {
+            throw new NotImplementedException();
         }
     }
 }
